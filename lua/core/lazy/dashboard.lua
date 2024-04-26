5d7e9c43c0e8ca46570a3733d5025a15e9c69e11
Benjamin Koerfer <benjamin.koerfer@t-online.de>
2024-04-26 20:19:13 +0200

    dashboard

--- lua/core/lazy/dashboard.lua
+++ lua/core/lazy/dashboard.lua
@@ -19,13 +19,13 @@ return {
 		}
 
 		dashboard.section.buttons.val = {
-			dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
-			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-			dashboard.button("u", "  Update plugins", ":Lazy sync<CR>"),
-			dashboard.button("r", "  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
-			dashboard.button("l", "  Open last session", "<cmd>RestoreSession<CR>"),
-			dashboard.button("q", "  Quit", ":qa<CR>"),
+			dashboard.button("f", "Find file", ":Telescope find_files<CR>"),
+			dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("c", "Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+			dashboard.button("u", "Update plugins", ":Lazy sync<CR>"),
+			dashboard.button("r", "Recently opened files", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("l", "Open last session", "<cmd>RestoreSession<CR>"),
+			dashboard.button("q", "Quit", ":qa<CR>"),
 		}
 
 		local handle, err = io.popen("fortune -s")
