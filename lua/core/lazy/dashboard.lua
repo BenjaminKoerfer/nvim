6398166fbbc3220b1492b6cbef94597ae3fbeee6
Benjamin Koerfer <benjamin.koerfer@t-online.de>
2024-04-26 21:12:19 +0200

    spelling

--- lua/core/lazy/dashboard.lua
+++ lua/core/lazy/dashboard.lua
@@ -15,7 +15,6 @@ return {
 			[[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
 			[[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
 			[[                                                                       ]],
-			[[                     λ it be like that sometimes λ                     ]],
 		}
 
 		dashboard.section.buttons.val = {
@@ -24,7 +23,6 @@ return {
 			dashboard.button("c", "Configuration", ":e ~/.config/nvim/init.lua <CR>"),
 			dashboard.button("u", "Update plugins", ":Lazy sync<CR>"),
 			dashboard.button("r", "Recently opened files", "<cmd>Telescope oldfiles<CR>"),
-			dashboard.button("l", "Open last session", "<cmd>RestoreSession<CR>"),
 			dashboard.button("q", "Quit", ":qa<CR>"),
 		}
 
