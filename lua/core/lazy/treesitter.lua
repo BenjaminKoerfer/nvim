b4266d17d7fb4bcb993b56d6bf9794c0f8fa1180
Fritz-Lakritz <benjamin.koerfer@t-online.de>
2024-04-29 10:20:09 +0200

    removed unnecessary code

--- lua/core/lazy/treesitter.lua
+++ lua/core/lazy/treesitter.lua
@@ -7,19 +7,10 @@ return { -- Highlight, edit, and navigate code
 		auto_install = true,
 		highlight = {
 			enable = true,
+			disable = { "latex" },
 			additional_vim_regex_highlighting = { "ruby" },
 		},
 		indent = { enable = true, disable = { "ruby" } },
-		highlight = {
-			enable = true,
-			disable = { "latex" },
-
-			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-			-- Using this option may slow down your editor, and you may see some duplicate highlights.
-			-- Instead of true it can also be a list of languages
-			additional_vim_regex_highlighting = false,
-		},
 	},
 	config = function(_, opts)
 		require("nvim-treesitter.install").prefer_git = true
