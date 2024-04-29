bd300c455daeed4a87de4efc9b007acdc471950d
Fritz-Lakritz <benjamin.koerfer@t-online.de>
2024-04-29 10:15:34 +0200

    removed TS auto update

--- lua/core/lazy/undotree.lua
+++ lua/core/lazy/undotree.lua
@@ -2,6 +2,6 @@ vim.g.maplocalleader = " "
 return {
 	"mbbill/undotree",
 	config = function()
-		vim.keymap.set("n", "<leader>t", "<cmd>UndotreeToggle<CR>")
+		vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>")
 	end,
 }
