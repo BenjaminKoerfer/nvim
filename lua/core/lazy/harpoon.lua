fa1b455f690b0e3351d0c132ba725b9447fe1cba
Fritz-Lakritz <benjamin.koerfer@t-online.de>
2024-04-30 09:09:23 +0200

    new commit
    description

--- lua/core/lazy/harpoon.lua
+++ lua/core/lazy/harpoon.lua
@@ -6,31 +6,7 @@ return {
 		branch = "harpoon2",
 		lazy = false,
 		requires = { "nvim-lua/plenary.nvim" }, -- if harpoon requires this
-		config = function()
-			require("harpoon").setup({})
-
-			-- local function toggle_telescope_with_harpoon(harpoon_files)
-			-- 	local file_paths = {}
-			-- 	for _, item in ipairs(harpoon_files.items) do
-			-- 		table.insert(file_paths, item.value)
-			-- 	end
-			--
-			-- 	require("telescope.pickers")
-			-- 		.new({}, {
-			-- 			prompt_title = "Harpoon",
-			-- 			finder = require("telescope.finders").new_table({
-			-- 				results = file_paths,
-			-- 			}),
-			-- 			previewer = require("telescope.config").values.file_previewer({}),
-			-- 			sorter = require("telescope.config").values.generic_sorter({}),
-			-- 		})
-			-- 		:find()
-			-- end
-			-- vim.keymap.set("n", "<leader>a", function()
-			-- 	local harpoon = require("harpoon")
-			-- 	toggle_telescope_with_harpoon(harpoon:list())
-			-- end, { desc = "Open harpoon window" })
-		end,
+		opts = {},
 		keys = {
 			{
 				"<leader>a",
