86bcf1c2528a3057c3a4e2644b6b95fd34940d8b
Benjamin Koerfer <benjamin.koerfer@t-online.de>
2025-01-27 19:48:34 +0100

    w11 A4 complete

--- lua/core/lazy/cmp.lua
+++ lua/core/lazy/cmp.lua
@@ -3,6 +3,29 @@ return { -- Autocompletion
 	event = "InsertEnter",
 	dependencies = {
 		-- Snippet Engine & its associated nvim-cmp source
+		{
+			"L3MON4D3/LuaSnip",
+			build = (function()
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- `friendly-snippets` contains a variety of premade snippets.
+				--    See the README about individual language/framework/plugin snippets:
+				--    https://github.com/rafamadriz/friendly-snippets
+				-- {
+				--   'rafamadriz/friendly-snippets',
+				--   config = function()
+				--     require('luasnip.loaders.from_vscode').lazy_load()
+				--   end,
+				-- },
+			},
+		},
 		"saadparwaiz1/cmp_luasnip",
 
 		-- Adds other completion capabilities.
@@ -16,6 +39,7 @@ return { -- Autocompletion
 		-- See `:help cmp`
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+		luasnip.config.setup({})
 
 		cmp.setup({
 			snippet = {
@@ -64,11 +88,6 @@ return { -- Autocompletion
 				["<S-Tab>"] = cmp.config.disable,
 			}),
 			sources = {
-				{
-					name = "lazydev",
-					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-					group_index = 0,
-				},
 				{ name = "luasnip" },
 				{ name = "nvim_lsp" },
 				{ name = "path" },
