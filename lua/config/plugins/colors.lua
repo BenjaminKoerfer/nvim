ff13b62bdf1f49b06e73737255ccffcec007a2f8
BenjaminKoerfer <benjamin.koerfer@t-online.de>
2025-03-18 15:31:37 +0100

    colors

--- lua/config/plugins/colors.lua
+++ lua/config/plugins/colors.lua
@@ -4,8 +4,13 @@ return {
     name = "rose-pine",
     config = function()
       vim.cmd.colorscheme "rose-pine"
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      local groups = {
+        "Normal", "NormalNC", "NormalFloat", "VertSplit", "WinSeparator",
+      }
+
+      for _, group in ipairs(groups) do
+        vim.api.nvim_set_hl(0, group, { bg = "none" })
+      end
     end,
   },
 }
