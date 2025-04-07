4c5a0dbaac92cf4704dab78985f8a53783dbbb2f
Benjamin Koerfer <benjamin.koerfer@t-online.de>
2025-04-07 11:26:36 +0200

    harpoon 2

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -1,19 +1,21 @@
 return {
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
     config = function()
+      local harpoon = require('harpoon')
       vim.keymap.set("n", "<leader>a", function()
-        require("harpoon.mark").add_file()
+        harpoon:list():add()
       end)
       vim.keymap.set("n", "<C-e>", function()
-        require("harpoon.ui").toggle_quick_menu()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
       end)
       for i = 1, 9, 1 do
         vim.keymap.set("n", string.format("<space>%d", i), function()
-          require("harpoon.ui").nav_file(i)
+          harpoon:list():select(i)
         end)
       end
     end,
--- lua/config/plugins/snippets.lua
+++ lua/config/plugins/snippets.lua
@@ -3,6 +3,12 @@ return {
     "L3MON4D3/LuaSnip",
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     build = "make install_jsregexp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
     config = function()
       local ls = require("luasnip")
       local s = ls.snippet
@@ -106,6 +112,9 @@ return {
             i(2, "max"),
           })
         ),
+      })
+      ls.add_snippets("javascript", {
+        s("test", t("Dies ist ein test")),
       })
     end,
   }
